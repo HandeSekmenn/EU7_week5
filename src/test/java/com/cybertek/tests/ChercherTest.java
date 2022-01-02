53cdfca78820208dc053f2c82c7c5ccc1586f682
HANDE <handesekmen7@gmail.com>
2022-01-02 17:18:41 +0100

    Task 2 completed

--- src/test/java/com/cybertek/tests/ChercherTest.java
+++ src/test/java/com/cybertek/tests/ChercherTest.java
@@ -1,7 +1,14 @@
 package com.cybertek.tests;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -18,6 +25,8 @@ Task1:
  */
 
     WebDriver driver;
+    
+    WebDriverWait wait;
 
     @BeforeMethod
     public void setUp(){
@@ -32,15 +41,39 @@ Task1:
 
     @AfterMethod
     public void tearDown(){
-        driver.close();
+       driver.close();
 
     }
 
     @Test
     public void alertPresentTest(){
+        //click on "click me",to open an alert after 5 seconds
+        WebElement initiateAlert = driver.findElement(By.id("alert"));
+        initiateAlert.click();
+        
+        wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.alertIsPresent());
+
+        //handle javascript alert
+        //if you don't wait you will get NoAlertPresentException
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
 
     }
 
+    @Test
+    public void disabledButtonTest(){
+        WebElement button = driver.findElement(By.id("disable"));
+        System.out.println("button.isEnabled() = " + button.isEnabled()); //false
+        WebElement buttonInitiator = driver.findElement(By.id("enable-button"));
+        buttonInitiator.click();
 
+        wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+
+        System.out.println("button.isEnabled() = " + button.isEnabled());
+        Assert.assertTrue(button.isEnabled(),"verify the Button is enabled");
+
+    }
 
 }
